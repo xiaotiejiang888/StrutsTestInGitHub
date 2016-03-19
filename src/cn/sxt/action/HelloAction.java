fdabc853fca2a3bc51f0e377fb81d539a5f8396e
xiaotiejiang888 <605113068@qq.com>
2016-03-19 22:22:49 +0800

    这又是一个新分支feature1

--- src/cn/sxt/action/HelloAction.java
+++ src/cn/sxt/action/HelloAction.java
@@ -7,12 +7,7 @@ import com.opensymphony.xwork2.*;
  * Created by Administrator on 2016/3/18.
  */
 public class HelloAction {
-    //test
-
-
-
-
-
+                 //Creating a new branch is quick AND simple.
     private String name;
     public  String execute(){
         System.out.println("name ="+name);
