dc8a71d2565c96f03475dc0e260286016c124be4
xiaotiejiang888 <605113068@qq.com>
2016-03-20 13:48:30 +0800

    小改一下

--- src/cn/sxt/action/HelloAction.java
+++ src/cn/sxt/action/HelloAction.java
@@ -7,7 +7,7 @@ import com.opensymphony.xwork2.*;
  * Created by Administrator on 2016/3/18.
  */
 public class HelloAction {
-    //dev上add 修改
+    //改bug 改了一半
     private String name;
     public  String execute(){
         System.out.println("name ="+name);
